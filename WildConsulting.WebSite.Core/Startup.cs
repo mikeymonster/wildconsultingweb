21051bb429fbad6ec9d55c581e1126efa411807b
Mike Wild <wildconsultingltd@googlemail.com>
2020-10-27 11:36:17 +0000

    Add Razor Runtime Compilation in dev

--- WildConsulting.WebSite.Core/Startup.cs
+++ WildConsulting.WebSite.Core/Startup.cs
@@ -8,18 +8,27 @@ namespace WildConsulting.WebSite.Core
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
-        }
+            Env = env;
+            }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Env { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+#if DEBUG
+            if (Env.IsDevelopment())
+            {
+                services.AddRazorPages().AddRazorRuntimeCompilation();
+            }
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
