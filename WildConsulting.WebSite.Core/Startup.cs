1a4337313df1055f847108b22355da86dac41693
Mike Wild <wildconsultingltd@googlemail.com>
2020-10-29 17:20:54 +0000

    Add HSTS and HTTPS Redirect

--- WildConsulting.WebSite.Core/Startup.cs
+++ WildConsulting.WebSite.Core/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,8 +18,6 @@ namespace WildConsulting.WebSite.Core
         public IConfiguration Configuration { get; }
         public IWebHostEnvironment Env { get; set; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             var mvcBuilder = services.AddControllersWithViews();
@@ -27,15 +26,28 @@ namespace WildConsulting.WebSite.Core
             {
                 mvcBuilder.AddRazorRuntimeCompilation();
             }
+            else
+            {
+                services.AddHsts(options =>
+                {
+                    options.MaxAge = TimeSpan.FromDays(365);
+                });
+            }
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                //app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
 
             app.UseStaticFiles();
 
