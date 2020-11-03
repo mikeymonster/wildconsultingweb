700851d2dd4eb3cf4318d93f770512bffba2a499
Mike Wild <wildconsultingltd@googlemail.com>
2020-11-03 11:44:24 +0000

    Try <requestFiltering removeServerHeader="true" />

--- WildConsulting.WebSite.Core/Program.cs
+++ WildConsulting.WebSite.Core/Program.cs
@@ -12,7 +12,6 @@ namespace WildConsulting.WebSite.Core
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseKestrel(options => options.AddServerHeader = false)
                 .UseStartup<Startup>();
     }
 }
